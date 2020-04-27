99f9f8b212215ab6e68d612057f22ec2af5bb305
Akshay Nanda <akshayn@sfu.ca>
2020-04-26 22:13:02 -0700

    flip cells around me passes me function now working

--- src/Board.js
+++ src/Board.js
@@ -62,6 +62,7 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    console.log("FLIPPING");
     let { ncols, nrows } = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -90,7 +91,13 @@ class Board extends Component {
       let row = [];
       for (let x = 0; x < this.props.ncols; x++) {
         let coord = `${y}-${x}`;
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />);
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
       }
       tableBoard.push(<tr>{row}</tr>);
     }
